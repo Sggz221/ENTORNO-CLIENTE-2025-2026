4c51890b7fc0c52ca7be0083a8586f70affd2c0b
Sggz221 <sggz221@gmail.com>
2025-09-28 19:48:19 +0200

    Funcion de validar dni y valdiar fechas terminada

--- 1 TRIMESTRE/ejercicio-dejovenes/js/validator.js
+++ 1 TRIMESTRE/ejercicio-dejovenes/js/validator.js
@@ -20,6 +20,63 @@ function validarString(cadena, min, max){
 	return res;
 } 
 
+function validarDni(dni) {
+    var regex = /^[0-9]{8}[A-Z]$/;
+    var letrasArray = "TRWAGMYFPDXBNJZSQVHLCKE";
+    
+    if (!regex.test(dni)) {
+        return false;
+    }
+
+    var numeroDni = parseInt(dni.substring(0, 8), 10);
+    var letraDni = letrasArray[numeroDni % 23];
+    
+    return letraDni === dni.substring(8); // la última letra
+}
+
+function validarFecha(fecha) {
+    // Formato correcto dd/mm/yyyy
+    var regex = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
+    if (!regex.test(fecha)) {
+        return false;
+    }
+
+    // Extraer partes de la fecha
+    var dia = parseInt(fecha.substring(0, 2), 10); // Indicando base decimal
+    var mes = parseInt(fecha.substring(3, 5), 10);
+    var year = parseInt(fecha.substring(6, 10), 10);
+
+    // Validar mes
+    if (mes < 1 || mes > 12) return false;
+
+    // Validar días segun el mes
+    if (mes === 2) { // Febrero
+		
+        if (dia < 1 || dia > 29) {
+			return false
+		};
+    } else if ([4, 6, 9, 11].includes(mes)) { 
+        if (dia < 1 || dia > 30) {
+			return false
+		};
+    } else {
+        if (dia < 1 || dia > 31) {
+			return false
+		};
+    }
+
+    // Validar que no sea superior a la fecha actual
+	var miFecha = new Date(year, mes-1, dia)
+    var fechaActual = new Date();
+
+ 	if (miFecha > fechaActual) {
+		return false
+	};
+
+    return true;
+}
+
+
 function validarEdad(cadena){
 	window.alert("pepe")
 	if(!isNaN(cadena) && cadena >= 16 && cadena <=32) {
